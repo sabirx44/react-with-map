3dc73771aed31003fce247b22934995def37dfb5
sabbirx44 <mdsabbirhossenx44@gmail.com>
2024-04-01 08:53:59 +0600

    Setup project

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+import './App.css'
+import GoogleMapReact from 'google-map-react';
+import { googleAPIKey } from './googleAPIKey';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+function App() {
+
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627
+    },
+    zoom: 11
+  };
+
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: googleAPIKey }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
+  );
+}
+
+export default App
